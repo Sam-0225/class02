0b6ae1f796c6831e44f6b0b8e6b147304eb5ee71
Sam <samuel951753@gmail.com>
2020-03-31 15:07:14 +0800

    Initial Commit

---
+++ Class02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Class02
+//
+//  Created by Sam on 2020/3/31.
+//  Copyright © 2020 Sam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
